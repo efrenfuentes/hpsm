8c065299beccc388f1be4f8a486000d0142fe75e
Efren Fuentes <efrenfuentes@gmail.com>
2014-07-02 15:17:16 -0430

    Config class

--- lib/hpsm/config.rb
+++ lib/hpsm/config.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'uri'
 
 module Hpsm
   class Config
@@ -16,12 +17,28 @@ module Hpsm
       raise ArgumentError, "Must specify a username" unless param_hash.has_key?(:username)
       raise ArgumentError, "Must specify a password" unless param_hash.has_key?(:password)
       raise ArgumentError, "Must specify a soap end point" unless param_hash.has_key?(:soap_end_point)
+      raise ArgumentError, "soap_end_point must be a valid url" unless (param_hash[:soap_end_point] =~ URI::regexp) == 0
 
       # Get params info
       @config = {}
       @config[:username] = param_hash[:username]
       @config[:password] = param_hash[:password]
       @config[:soap_end_point] = param_hash[:soap_end_point]
+    end
+
+    # get methods for properties
+    %w(username password soap_end_point).each do |meth|
+      define_method('get_' + meth) { @config[meth.to_sym] }
+    end
+
+    # set methods for properties
+    %w(username password).each do |meth|
+      define_method('set_' + meth) { |value| @config[meth.to_sym] = value }
+    end
+
+    def set_soap_end_point(value)
+      raise ArgumentError, "soap_end_point must be a valid url" unless (value =~ URI::regexp) == 0
+      @config[:soap_end_point] = value
     end
   end
 end
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -1,6 +1,11 @@
 require 'spec/spec_helper'
 
 describe Config do
+  before(:all) do
+    @username = 'username'
+    @password = 'password'
+    @soap_end_point = 'http://host/table.wsdl'
+  end
 
   describe '#new' do
     it 'should accept a config file' do
@@ -9,7 +14,7 @@ describe Config do
     end
 
     it 'should accept a config hash' do
-      conf = Hpsm::Config.new :username => 'user', :password => 'password', :soap_end_point => 'soap'
+      conf = Hpsm::Config.new :username => @username, :password => @password, :soap_end_point => @soap_end_point
       expect(conf).to_not be_nil
     end
 
@@ -19,15 +24,73 @@ describe Config do
     end
 
     it 'should have username' do
-      expect { conf = Hpsm::Config.new :password => 'password', :soap_end_point => 'soap' }.to raise_error(ArgumentError)
+      expect { conf = Hpsm::Config.new :password => @password, :soap_end_point => @soap_end_point }.to raise_error(ArgumentError)
     end
 
     it 'should have password' do
-      expect { conf = Hpsm::Config.new :username => 'user', :soap_end_point => 'soap' }.to raise_error(ArgumentError)
+      expect { conf = Hpsm::Config.new :username => @username, :soap_end_point => @soap_end_point }.to raise_error(ArgumentError)
     end
 
     it 'should a soap end point' do
-      expect {  conf = Hpsm::Config.new :username => 'user', :password => 'password' }.to raise_error(ArgumentError)
+      expect {  conf = Hpsm::Config.new :username => @username, :password => @password }.to raise_error(ArgumentError)
+    end
+
+    it 'soap end point must be a valid url' do
+      expect { conf = Hpsm::Config.new :username => @username, :password => @password, :soap_end_point => 'bad soap end point' }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe 'Properties' do
+    before(:each) do
+      @conf = Hpsm::Config.new :username => @username, :password => @password, :soap_end_point => @soap_end_point
+    end
+
+    it 'have username methods' do
+      expect(@conf).to respond_to(:get_username)
+      expect(@conf).to respond_to(:set_username)
+    end
+
+    it 'have password methods' do
+      expect(@conf).to respond_to(:get_password)
+      expect(@conf).to respond_to(:set_password)
+    end
+
+    it 'have soap end point methods' do
+      expect(@conf).to respond_to(:get_soap_end_point)
+      expect(@conf).to respond_to(:set_soap_end_point)
+    end
+
+    it 'get username' do
+      expect(@conf.get_username).to eq(@username)
+    end
+
+    it 'get password' do
+      expect(@conf.get_password).to eq(@password)
+    end
+
+    it 'get soap end point' do
+      expect(@conf.get_soap_end_point).to eq(@soap_end_point)
+    end
+
+    it 'set username' do
+      @conf.set_username('new_username')
+      expect(@conf.get_username).to eq('new_username')
+    end
+
+    it 'set password' do
+      @conf.set_password('new_password')
+      expect(@conf.get_password).to eq('new_password')
+    end
+
+    describe 'set soap end point' do
+      it 'accept valid soap end point' do
+        new_soap_end_point = 'http://other-host/table2.wsdl'
+        @conf.set_soap_end_point(new_soap_end_point)
+        expect(@conf.get_soap_end_point).to eq(new_soap_end_point)
+      end
+      it 'raise error for invalid soap end point' do
+        expect { @conf.set_soap_end_point('invalid_soap_end_point') }.to raise_error(ArgumentError)
+      end
     end
   end
 end
